87accbeab7e7de87ae373c4f16659b8511e912a9
Miraz <w3miraj@gmail.com>
2024-01-30 00:29:07 +0600

    Project

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//----------------------- output-------------
+// alert("linked")
+// document.write(" <h2>Bangladesh</h2> ")
+// document.getElementById("one") .innerHTML ="Bangladesh"
+// document.getElementsByClassName ("one") [0] .innerHTML="Bangladesh"
+// document.getElementsByTagName("h2")[0] .innerHTML ="Bangladesh"
+// console.log("Bangladesh");
+
+
+//--------------- Type --------------
+// number
+// string
+// object
+// arry object
+// boolean
+// undefined
+// null
+// 
+
+
+//---------------------- variable ----------------
+// var
+// let
+// const
+
+
+// var x = 20
+// var x = 30
+
+// let x = 20
+//     x = 40
+
+// const x = 20
+
+// var x = 30
+
+// console.log(x);
+
+// for(i=1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(i=2024; i >= 1970; i--){
+//     console.log(i);
+// }
